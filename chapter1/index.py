0ca66793d75be6e7ac562dc5cd7a465e31c58154
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-13 19:31:59 +0700

    Create index.py

---
+++ chapter1/index.py
@@ -0,0 +1,47 @@
+# penulisan statement python
+# --------------------------
+# statement adalah sebuah intruksi atau kalimat perintah yang akan dieksekusi oleh komputer
+# penulisan satu statement tidak diakhiri dengan tanda titik-koma
+
+# contoh
+print("hello world")
+
+# penulisan string pada python
+# ----------------------------
+# string adalah kumpulan dari karakter
+# string dalam pemrograman biasanya ditulis dengan dibungkus menggunakan tanda petik tunggal maupun ganda
+
+# contoh
+namaMahasiswa = "Sony"
+mataKuliah = "Pemrograman Berbasis Open Source"
+print(namaMahasiswa)
+print(mataKuliah)
+
+# penulisan case style pada python
+# --------------------------------
+# 1. gaya snake case: cirinya menggunakan garis bawah (_) untuk memisahkan suku kata
+# 2. gaya camel case: cirinya semua suku kata menyatu dan terdapat huruf kapital untuk memisahnya
+
+# contoh
+ini_penulisan_snake_case() # snake case
+namaVariable() # camel case
+
+# penulisan blok program python
+# -----------------------------
+# blok program adalah kumpulan dari beberapa statement yang digabung dalam 1 blok
+# penulisan blok program harus ditambahkan indentasi (tab atau spasi 2x/4x)
+
+# contoh
+if nama == 'petani kode' :
+  print(nama)
+  print("selamat datang")
+print("world")
+
+# penulisan komentar pada python
+# ------------------------------
+# komentar merupakan baris kode yang tidak akan dieksekusi oleh program
+# komentar digunakan untuk memberikan informasi tambahan dan untuk menonaktifkan kode
+# penulisan komentar pada pemrograman menggunakan tanda #
+
+# contoh
+# ini adalah komentar
