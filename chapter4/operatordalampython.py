0a5eda16ba27b1c7343d043f40dc10da483a8ef9
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-15 19:27:03 +0700

    Create operatordalampython.py

---
+++ chapter4/operatordalampython.py
@@ -0,0 +1,45 @@
+# pengertian operand dan operator
+# -------------------------------
+# operand adalah nilai asal yang dipakai dalam sebuah proses operasi
+# operator adalah instruksi yang diberikan untuk mendapatkan hasil dari proses tersebut
+
+# contoh 
+# 10 + 2. angka 10 dan 2 disebut sebagai operand, sedangkan tanda tambah (+) adalah operator
+
+# jenis-jenis operator dalam python
+# ---------------------------------
+# operator aritmatika
+# operator perbandingan
+# operator logika
+# operator bitwise
+# operator assignment
+# operator identitas
+# operator keanggotaan
+# operator ternary
+
+# operator aritmatika
+# -------------------
+# +, -, *, /, %, **
+
+# contoh
+a = 10
+b = 2
+
+# menggunakan operator penjumlahan
+c = a + b
+print("hasil",a," + ",b," = ",c)
+# menggunakan operator pengurangan
+c = a - b
+print("hasil",a," - ",b," = ",c)
+# menggunakan operator perkalian
+c = a * b
+print("hasil",a," * ",b," = ",c)
+# menggunakan operator pembagian
+c = a / b
+print("hasil",a," / ",b," = ",c)
+# menggunakan operator sisa bagi
+c = a % b
+print("hasil",a," % ",b," = ",c)
+# menggunakan operator pangkat
+c = a ** b
+print("hasil",a," ** ",b," = ",c)
