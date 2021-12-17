5053a0137744d97a7b9ca2b665861151cc967a25
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-17 13:42:51 +0700

    Update operatordalampython.py

--- chapter4/operatordalampython.py
+++ chapter4/operatordalampython.py
@@ -170,3 +170,57 @@ print (aku)
 jomblo = True
 status = ("menikah", "single")[jomblo]
 print (status)
+
+# urutan evaluasi
+# ---------------
+# urutan evaluasi merupakan urutan operator yang dikerjakan terlebih dahulu
+# untuk urutannya silahkan search di web
+
+# mengubah urutan evaluasi
+# ------------------------
+# untuk membuat ekspresi lebih mudah dibaca, kita dapat menggunakan tanda kurung
+# sebagai contoh, 2 + (3 * 4) lebih mudah dipahami daripada 2 + 3 * 4
+# pemakaian tanda kurung jangan terlalu berlebihan seperti (2 + (3 * 4))
+
+# contoh
+hasil = 2 + 3 * 4
+print ("2 + 3 * 4 = %s" % hasil) # 14
+hasil = (2 + 3) * 4
+print ("2 + 3) * 4 = %s" % hasil) # 20
+hasil = 2 / 3 * 4
+print ("2 / 3 * 4 = %s" % hasil) # 2,666...
+hasil = 2.0 / 3 * 4
+print ("2.0 / 3 * 4 = %s" % hasil) # 2,666...
+
+# sifat asosiatif
+# ---------------
+# operator dengan level urutan evaluasi yang sama akan dievaluasi dari kiri ke kanan
+# sebagai contoh 2 + 3 + 4 akan dievaluasi sebagai (2 + 3) + 4
+# beberapa operator seperti pengisian nilai (assignment) mempunyai sifat asosiatif dari kanan ke kiri, contoh: a = b = c akan dievaluasi a = (b = c)
+
+# operator identitas dan keanggotaan
+# ----------------------------------
+# kedua operator ini kadang disebut juga sebagai operator khusus (special operator), karena tidak selalu tersedia di bahasa pemrograman lain
+
+# operator identitas
+# ------------------
+# operator identitas adalah operator yang bisa dipakai untuk memeriksa apakah nilai sebuah variabel ada di tempat yang sama (di memory) atau tidak
+# operator ini terdiri dari 2 jenis: yaitu is dan is not
+# is: bernilai True jika kedua operand merujuk ke object yang sama dan berisi nilai yang sama
+# is not: bernilai True jika kedua operan merujuk ke objek yang tidak sama
+
+# contoh
+a = 5
+b = 5
+c = 6
+print("a is b :", a is b) # True
+print("a is c :", a is c) # False
+print("a is not c :", a is not c) # True
+i = "STMIK PONTIANAK"
+j = "STMIK PONTIANAK"
+print("i is j :", i is j) # False, meskipun nilai element-nya sama persis, tapi python menyimpannya di alamat memory yang berbeda, sehingga dianggap tidak identik
+print("i is not j :", i is not j) # True
+x = ["a","b","c"]
+y = ["a","b","c"]
+print("x is y :", x is y) # False
+print("x is not y :", x is not y) # True
