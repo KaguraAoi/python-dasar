6fceae2d419cfe27abe2f02788240f6ad5d63f79
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-23 12:18:12 +0700

    Update strukturpercabangan.py

--- chapter5/strukturpercabangan.py
+++ chapter5/strukturpercabangan.py
@@ -69,3 +69,76 @@ if a < b : # True
 else:
   print("nilai terbesar adalah ", a)
   print("nilai terkecil adalah ", b)
+
+# struktur percabangan if else if else
+# ------------------------------------
+# percabangan if else if else digunakan apabila terdapat lebih dari dua kondisi
+# jika kondisi if bernilai False, maka program akan lanjut ke kondisi else if. jika else if juga tidak terpenuhi maka akan lanjut ke else if selanjutnya (tergantung jumlah else if yang diberikan) sehingga blok else terakhir bernilai True
+# dalam bahasa python. perintah else if ditulis menjadi elif
+
+# contoh 1
+# --------
+# nilai = 'D'
+
+if nilai == 'A' :
+	print("pertahankan terus")
+else if nilai == 'B' :
+	print("harus lebih baik lagi")
+else if nilai == 'C' :
+	print("perbanyak belajar lagi")
+else if nilai == 'D' :
+	print("jangan keseringan main game") # True
+else if nilai == 'E' :
+	print("keseringan bolos")
+else :
+	print("maaf, format nilai yang anda berikan tidak sesuai")
+	
+# switch case
+# -----------
+# dalam bahasa python tidak tersedia struktur switch case
+# jadi untuk kondisi yang cukup banyak, kita harus menggunakan if elif else atau menggunakan beberapa trik yang melibatkan function untuk pengganti switch case
+
+# looping bersarang if
+# --------------------
+# looping bersarang adalah statement if yang didalamnya terdapat if lagi
+# jika pernyataan if yang paling luar bernilai True, maka kondisi if yang berada dalamnya baru akan dilihat oleh sistem.
+
+# contoh 
+# ------
+bekerja = True
+umur = 25
+
+if umur >= 25 : # True
+	print("sudah dewasa")
+	if bekerja : # True
+		print("siap menikah")
+	else :
+		print("belum siap menikah")
+else :
+	print("masih bocah")
+	if bekerja :
+		print("tabung buat menikah")
+	else :
+		print("kerja dulu sana")
+
+# short hand if
+# -------------
+# short hand if digunakan apabila hanya membutuhkan satu if tanpa elif atau else
+
+# contoh
+# ------
+a = 7
+b = 5
+
+if (a > b) : print("{} lebih besar dari {}".format(a, b)) # 7 lebih besar dari 5
+  
+# short hand if else
+# ------------------
+# else juga bisa dibuatkan short hand nya
+
+# contoh
+# ------
+a = 18
+b = 23
+
+print("a lebih besar dari b") if (a > b) else print("a lebih kecil dari b") # a lebih kecil dari b
