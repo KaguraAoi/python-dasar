137ad6758488b87946ab2ae6b8f18431320e1d9c
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-26 18:42:09 +0700

    Update variabeldantipedata.py

--- chapter2/variabeldantipedata.py
+++ chapter2/variabeldantipedata.py
@@ -5,10 +5,12 @@
 # variabel nilainya bisa berubah-ubah
 
 # membuat variabel
+# ----------------
 # variabel di python dapat dibuat dengan format seperti ini:
 # nama_variabel = [nilai]
 
 # contoh
+# ------
 nilai_ujian = 87.5
 nama = "Aditya"
 lulus = False
@@ -17,7 +19,8 @@ lulus = False
 # -------------------------
 # nama variabel boleh diawali menggunakan huruf atau garis bawah (_), contoh: nama, _nama, namaKu, nama_variabel
 # karakter selanjutnya dapat berupa huruf, garis bawah (_) atau angka, contoh: __nama, n2, nilai1
-# karakter pada nama variabel bersifat case-sensitif. artinya huruf besar dan kecil dibedakan. misalnya, variabel_Ku dan variabel_ku, keduanya adalah variabel yang berbeda
+# karakter pada nama variabel bersifat case-sensitif. artinya huruf besar dan kecil dibedakan
+# misalnya, variabel_Ku dan variabel_ku, keduanya adalah variabel yang berbeda
 # nama variabel tidak boleh menggunakan kata kunci yang sudah ada dalam python seperti if, while, for, dsb
 
 # tipe data
@@ -26,6 +29,7 @@ lulus = False
 # untuk memeriksa tipe data pada suatu variabel, kita bisa menggunakan fungsi type()
 
 # contoh
+# ------
 jumlah = 2
 ketelitian = 20.25
 merk = "Xiao Mi"
@@ -44,6 +48,7 @@ merk = "Xiao Mi"
 # float, contoh 1.3, 4.2, 22.3
 
 # contoh
+# ------
 harga = 120000000
 berat = 23.12
 
@@ -52,12 +57,12 @@ berat = 23.12
 # tipe data teks dibagi menjadi dua jenis lagi:
 # char, contoh 'R'
 # string, contoh "aku lagi makan"
-# penulisan tipe data teks harus diapit dengan tanda petik. bisa menggunakan petik tunggal (' '), ganda ('' ''), dan tiga (''' ''')
+# penulisan tipe data teks harus diapit dengan tanda petik. bisa menggunakan petik tunggal(' ') atau ganda('' '')
 
 # contoh
+# ------
 A = 'HP'
 B = ''SMARTPHONE ANDROID''
-C = '''IPHONE 12 PRO MAX'''
 
 # tipe data boolean
 # -----------------
@@ -65,12 +70,14 @@ C = '''IPHONE 12 PRO MAX'''
 # penulisan True dan False, huruf pertamanya harus kapital dan tanpa tanda petik
 
 # contoh
+# ------
 bergerak = False
 nyala = 0
 
 # konversi tipe data
 # ------------------
-# meskipun python telah otomatis mendeteksi tipe data yang tersimpan dalam variabel, tapi ada kalanya kita perlu melakukan konversi tipe data
+# konversi digunakan apabila kita ingin mengubah suatu tipe data ke tipe data yang lain
+# misalnya kita ingin mengubah input string menjadi int
 # fungsi-fungsi untuk mengubah tipe data:
 # int() untuk mengubah menjadi integer
 # long() untuk mengubah menjadi integer panjang
@@ -83,6 +90,7 @@ nyala = 0
 # oct() untuk mengubah menjadi bilangan okta
 
 # contoh
+# ------
 data = 212
 st = str(data)
 fl = float(data)
