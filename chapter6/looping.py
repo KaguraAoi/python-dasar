a15b0e34c44d957223bcdd9065945ca8a6d25f80
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2021-12-26 18:29:08 +0700

    Update looping.py

--- chapter6/looping.py
+++ chapter6/looping.py
@@ -10,7 +10,6 @@
 
 # sintaks for
 # -----------
-
 for var in sequence :
   body of for
   
@@ -20,8 +19,35 @@ for var in sequence :
 
 # contoh
 # ------
-
 angka = [7, 8, 5, 3] # ini adalah sequence
 
 for a in angka : # a adalah variabel dan angka adalah sequence
-  print(a)
+  print(a) # hasilnya: 7 8 5 3
+  
+item = ['kopi', 'jus', 'teh']
+
+for isi in item :
+  print(isi) # hasil: kopi jus teh
+  
+# fungsi range()
+# --------------
+# fungsi range() digunakan untuk menghasilkan deret bilangan
+# misalkan range(10) maka akan menghasilkan bilangan dari 0 sampai dengan 9 (10 bilangan)
+# kita juga bisa menentukan batas bawah, batas atas, dan interval dengan format range(batas bawah, batas atas, interval). bila dikosongkan, maka nilai default 1 yang kan digunakan
+
+# contoh
+# ------
+print(range(10)) # hasil: range(0, 10)
+print(list(range(10))) # hasil: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(2,8))) # hasil: [2, 3, 4, 5, 6, 7]
+print(list(range(2, 20, 3))) # hasil: [2, 5, 8, 11, 14, 17]
+
+# contoh 2
+# --------
+for i in range(0, 10, 2) :
+  print(i) # hasil: 0 2 4 6 8
+  
+ulang = 10
+
+for i in range(ulang) :
+  print("perulangan ke-" + str(i)) # hasil: perulangan ke-1 perulangan ke-... perulangan ke-9
