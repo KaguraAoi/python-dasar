97b12addedd27b91372f5eee9dd850a851d1301c
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2022-01-01 13:34:20 +0700

    Update strukturpercabangan.py

--- chapter5/strukturpercabangan.py
+++ chapter5/strukturpercabangan.py
@@ -1,6 +1,6 @@
-# apa itu percabangan dan kenapa dinamakan percabangan?
-# -----------------------------------------------------
-# untuk menggambarkan alur program yang bercabang
+# apa itu percabangan?
+# ---------------------
+# percabangan digunakan untuk menggambarkan alur program yang bercabang
 # percabangan akan menentukan tindakan sesuai dengan logika/kondisi yang kita berikan
 
 # struktur percabangan if
@@ -9,7 +9,7 @@
 # bagian condition berperan sebagai penentu dari struktur percabangan 
 # jika condition benar(True), blok kode program akan dijalankan
 # jika condition tidak benar(False), blok kode program tidak akan dijalankan
-# blok kode program dalam bahasa Python ditandai dengan (:) setelah penulisan condition, 
+# blok kode program dalam bahasa python ditandai dengan (:) setelah penulisan condition, 
 # kemudian diikuti satu atau beberapa baris dengan awalan indentasi
 
 # contoh 1
@@ -18,52 +18,52 @@ a = 12
 b = 5
 
 if a > b :
-  print("variabel a lebih besar dari variabel b") # variabel a lebih besar dari variabel b
+  print("variabel a lebih besar dari variabel b") # True, maka blok program dijalankan
   
 # contoh 2
 # --------
-lulus = input("apakah kamu lulus? [ya/tidak]: ")
+lulus = input("apakah kamu lulus? [ya/tidak] : ") # tidak
 
 if (lulus == "tidak") :
-  print("kamu harus ikut ujian") # jika tidak maka jawabannya "kamu harus ikut ujian"
+  print("kamu harus ikut ujian") # jika True maka blok program dijalankan
     
 # contoh 3
 # --------
 a = 9
     
 if (a % 2 == 0) :
-  print(a, "merupakan bilangan genap")
+  print(a, " merupakan bilangan genap")
 if (a % 2 != 0) :
-  print(a, "merupakan bilangan ganjil") # 9 merupakan bilangan ganjil
+  print(a, " merupakan bilangan ganjil") # True, 9 merupakan bilangan ganjil
     
 # struktur percabangan if else
 # ----------------------------
-# percabangan if else digunakan saat terdapat dua pilihan keputusan
-# pada dasarnya, kondisi if else merupakan modifikasi tambahan dari kondisi if
+# percabangan else digunakan saat terdapat dua pilihan keputusan
+# pada dasarnya, kondisi else merupakan modifikasi tambahan dari kondisi if
 # saat if bernilai False maka bagian else yang akan dijalankan
 
 # contoh 1
 # --------
-umur = int(input("berapa umur kamu: "))
+umur = int(input("berapa umur kamu : ")) # misal 20
 
 if umur >= 18 :
-  print("kamu boleh membuat SIM")
-else :
-  print("Kamu belum boleh membuat SIM")
+  print("kamu boleh membuat SIM") # jika True maka blok program dijalankan
+else : 
+  print("Kamu belum boleh membuat SIM") # jika False maka jalankan ini
 
 # contoh 2
 # --------
 a = 10
 
 if (a % 2 == 0) :
-  print(a, "merupakan bilangan genap") # a merupakan bilangan genap
+  print(a, "merupakan bilangan genap") # True, 10 merupakan bilangan genap
 else :
   print(a, "merupakan bilangan ganjil") 
   
 # contoh 3
 # --------
-a = int(input("masukkan nilai a : ")) # 5
-b = int(input("masukkan nilai b : ")) # 7
+a = int(input("masukkan nilai a : ")) # misal 5
+b = int(input("masukkan nilai b : ")) # misal 7
 
 if a < b : # True
   print("nilai terbesar adalah ", b) # 7
@@ -72,13 +72,12 @@ else:
   print("nilai terbesar adalah ", a)
   print("nilai terkecil adalah ", b)
 
-# struktur percabangan if else if else
-# ------------------------------------
-# percabangan if else if else digunakan apabila terdapat lebih dari dua kondisi
-# jika kondisi if bernilai False, maka program akan lanjut ke kondisi else if
-# jika else if juga tidak terpenuhi maka akan lanjut ke else if selanjutnya
-# (tergantung jumlah else if yang diberikan) sehingga blok else terakhir bernilai True
-# dalam bahasa python. perintah else if ditulis menjadi elif
+# struktur percabangan elif (else if)
+# -----------------------------------
+# percabangan elif digunakan apabila terdapat lebih dari dua kondisi
+# jika kondisi if bernilai False, maka program akan lanjut ke kondisi elif
+# jika elif juga tidak terpenuhi maka akan lanjut ke elif selanjutnya
+# (tergantung jumlah elif yang diberikan) sehingga blok else terakhir bernilai True
 
 # contoh 1
 # --------
@@ -86,13 +85,13 @@ else:
 
 if nilai == 'A' :
   print("pertahankan terus")
-else if nilai == 'B' :
+elif nilai == 'B' :
   print("harus lebih baik lagi")
-else if nilai == 'C' :
+elif nilai == 'C' :
   print("perbanyak belajar lagi")
-else if nilai == 'D' :
+elif nilai == 'D' :
   print("jangan keseringan main game") # True
-else if nilai == 'E' :
+elif nilai == 'E' :
   print("keseringan bolos")
 else :
   print("maaf, format nilai yang anda berikan tidak sesuai")
@@ -135,10 +134,10 @@ else :
 a = 7
 b = 5
 
-if (a > b) : print("{} lebih besar dari {}".format(a, b)) # 7 lebih besar dari 5
+if (a > b) : print("{} lebih besar dari {}".format(a, b)) # True, 7 lebih besar dari 5
   
-# short hand if else
-# ------------------
+# short hand else
+# ---------------
 # else juga bisa dibuatkan short hand nya
 
 # contoh
