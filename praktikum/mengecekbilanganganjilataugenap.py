b81c571e58f6856ab03b630afe01a23f049b9ef3
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2022-01-06 12:51:34 +0700

    Create mengecekbilanganganjilataugenap.py

---
+++ praktikum/mengecekbilanganganjilataugenap.py
@@ -0,0 +1,10 @@
+# program menentukan bilangan suatu bilangan ganjil atau genap
+
+# input bilangan
+bilangan = int(input("masukkan angka: "))
+
+# cek kondisi
+if bilangan % 2 == 0 :
+  print("{} adalah bilangan genap".format(bilangan))
+elif bilangan % 2 == 1 :
+  print("{} adalah bilangan ganjil".format(bilangan)) 
