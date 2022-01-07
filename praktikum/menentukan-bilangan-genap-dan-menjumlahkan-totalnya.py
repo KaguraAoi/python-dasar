2dff275c05fb8de2f0f08af5ff073ed833fc1387
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2022-01-07 10:18:53 +0700

    Create menentukan-bilangan-genap-dan-menjumlahkan-totalnya.py

---
+++ praktikum/menentukan-bilangan-genap-dan-menjumlahkan-totalnya.py
@@ -0,0 +1,11 @@
+# contoh program menentukan bilangan genap dan menjumlahkan totalnya
+
+tmp = 0
+
+# input bilangan
+for i in range (10) :
+  bil = int(input("masukkan bilangan: "))
+  tmp = tmp + bil # nilai yang sudah disimpan ke tmp akan dijumlahkan
+  # dengan tmp yang baru
+print("------------------------")
+print("total : {}".format(tmp))
