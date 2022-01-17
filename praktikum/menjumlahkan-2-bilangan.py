562cad3b3df9ef4f2b9f6782777844093ca97143
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2022-01-17 19:46:43 +0700

    Update menjumlahkan-2-bilangan.py

--- praktikum/menjumlahkan-2-bilangan.py
+++ praktikum/menjumlahkan-2-bilangan.py
@@ -1,8 +1,8 @@
 # program untuk menjumlahkan 2 bilangan
 
 # user menginput bilangan
-bilangan1 = int(input("masukkan bilangan pertama: "))
-bilangan2 = int(input("masukkan bilangan kedua: "))
+bilangan1 = int(input("masukkan bilangan pertama : "))
+bilangan2 = int(input("masukkan bilangan kedua : "))
 
 # operasi penjumlahan
 penjumlahan = bilangan1 + bilangan2
