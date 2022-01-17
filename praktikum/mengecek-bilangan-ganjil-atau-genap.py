1c90196eb8b539102581d97cfdc7a947dbbd1e00
Kagura Aoi <93599305+KaguraAoi@users.noreply.github.com>
2022-01-17 19:45:47 +0700

    Update mengecek-bilangan-ganjil-atau-genap.py

--- praktikum/mengecek-bilangan-ganjil-atau-genap.py
+++ praktikum/mengecek-bilangan-ganjil-atau-genap.py
@@ -1,7 +1,7 @@
 # program menentukan bilangan suatu bilangan ganjil atau genap
 
 # input bilangan
-bilangan = int(input("masukkan angka: "))
+bilangan = int(input("masukkan angka : "))
 
 # cek kondisi
 if bilangan % 2 == 0 :
